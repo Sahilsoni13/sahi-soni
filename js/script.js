18ae6e1c5a20761710e85be26a1006d3a8e42a40
Sahilsoni13 <135135494+Sahilsoni13@users.noreply.github.com>
2023-06-02 14:23:11 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let burger =document.querySelector(".burger");
+let navbar = document.querySelector(".navbar");
+
+burger.addEventListener("click", ()=>{
+    // burger.classList.toggle("active");
+    navbar.classList.toggle("active");
+})
